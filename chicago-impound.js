ee1c5e36094011e421c998e71046ee507c5a7022
Alex Knoll <alexandergknoll@gmail.com>
2015-04-23 13:39:01 -0500

    MVP

--- chicago-impound.js
+++ chicago-impound.js
@@ -1,19 +1,57 @@
 var chicagoImpoundApiURL = "https://data.cityofchicago.org/resource/ygr5-vcbg.json";
 
 $(function(){
-	var plateSearchForm = $('.platesearch')
+	var searchResults, plateSearchForm;
 
-	plateSearchForm.on('submit', carRequest)
-})
+	plateSearchForm = $('.searchform');
+	searchResults = $('.searchtable');
 
-function carRequest (e) {
-	var formData;
-	e.preventDefault();
+	plateSearchForm.on('submit', carRequest);
 
-	formData = $(this).serialize();
- 	$.getJSON(chicagoImpoundApiURL, formData).done(carResponse);
+	function carRequest (e) {
+		var formData;
+		e.preventDefault();
+
+		formData = $(this).serialize();
+	 	$.getJSON(chicagoImpoundApiURL, formData).done(carResponse);
 	}
 
-function carResponse (response) {
-	console.log(response);
-}
+	function carResponse (cars) {
+		clearResults();
+		cars.forEach(appendCarResult);
+	}
+
+	function clearResults () {
+		searchResults.find('.result').remove();
+	}
+
+	function appendCarResult (car) {
+		htmlCarResult(car).appendTo(searchResults)
+	}
+
+	function htmlCarResult (car) {
+		var result;
+
+		console.log(car);
+
+		result = $('<tr class="result">').append(
+				$('<td>').text(car.tow_date.substring(0,10))
+			).append(
+				$('<td>').text(car.plate)
+			).append(
+				$('<td>').text(car.state)
+			).append(
+				$('<td>').text(car.make)
+			).append(
+				$('<td>').text(car.color)
+			).append(
+				$('<td>').text(car.style)
+			).append(
+				$('<td>').append(
+					$('<a class="resultlink" href="#">').text("Hey That's My Car!")
+				)
+			)
+		return result;
+	}
+
+})
