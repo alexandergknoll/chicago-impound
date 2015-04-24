f3bd369563a3435cb17bc7eaca7b57550660c135
Alex Knoll <alexandergknoll@gmail.com>
2015-04-23 21:37:32 -0500

    refactor

--- chicago-impound.js
+++ chicago-impound.js
@@ -1,57 +1,98 @@
+// jshint jquery:true
+
 var chicagoImpoundApiURL = "https://data.cityofchicago.org/resource/ygr5-vcbg.json";
 
 $(function(){
-	var searchResults, plateSearchForm;
+	var doc, newCell, searchResults, resultDetail, plateSearchForm;
 
+	doc = $(document);
+	newCell = $('<td>');
+	searchResults = $('.searchresults');
+	resultDetail = $('.resultdetail');
 	plateSearchForm = $('.searchform');
-	searchResults = $('.searchtable');
 
 	plateSearchForm.on('submit', carRequest);
+	doc.on('click','.resultlink', showInventory);
 
 	function carRequest (e) {
-		var formData;
 		e.preventDefault();
 
-		formData = $(this).serialize();
+		var formData = $(this).serialize();
 	 	$.getJSON(chicagoImpoundApiURL, formData).done(carResponse);
 	}
 
+	function showInventory(e) {
+		e.preventDefault();
+
+		var inventoryNumber = $(this).attr("inventory_number");
+		var towedToAddress = $(this).attr("towed_to_address");
+		var towFacilityPhone = $(this).attr("tow_facility_phone");
+
+		clearResultDetail();
+		appendResultDetail(inventoryNumber, towedToAddress, towFacilityPhone);
+		showResultDetail();
+	}
+
 	function carResponse (cars) {
-		clearResults();
+		clearSearchResults();
+		showSearchResults();
 		cars.forEach(appendCarResult);
 	}
 
-	function clearResults () {
+	function clearSearchResults () {
 		searchResults.find('.result').remove();
 	}
 
+	function clearResultDetail() {
+		resultDetail.text("");
+	}
+
+	function showSearchResults () {
+		searchResults.show();
+	}
+
+	function showResultDetail () {
+		resultDetail.show();
+	}
+
 	function appendCarResult (car) {
-		htmlCarResult(car).appendTo(searchResults)
+		htmlCarResult(car).appendTo(searchResults);
+	}
+
+	function appendResultDetail (inventoryNumber, towedToAddress, towFacilityPhone) {
+		resultDetail
+			.append($('<p>')
+				.text(inventoryNumber))
+			.append($('<p>')
+				.text(towedToAddress))
+			.append($('<p>')
+				.text(towFacilityPhone));
 	}
 
 	function htmlCarResult (car) {
-		var result;
+		var $result = $('<tr class="result">');
 
-		console.log(car);
+		$result
+				.append($('<td>').text(car.tow_date.substring(0,10)))
+				.append($('<td>').text(car.plate))
+				.append($('<td>').text(car.state))
+				.append($('<td>').text(car.make))
+				.append($('<td>').text(car.color))
+				.append($('<td>').text(car.style))
+				.append($('<td>')
+					.append($('<a>')
+						.text("Hey That's My Car!")
+						.attr({
+							class: "resultlink",
+							href: "#",
+							inventory_number: car.inventory_number,
+							towed_to_address: car.towed_to_address,
+							tow_facility_phone: car.tow_facility_phone
+						})
+					)
+				);
 
-		result = $('<tr class="result">').append(
-				$('<td>').text(car.tow_date.substring(0,10))
-			).append(
-				$('<td>').text(car.plate)
-			).append(
-				$('<td>').text(car.state)
-			).append(
-				$('<td>').text(car.make)
-			).append(
-				$('<td>').text(car.color)
-			).append(
-				$('<td>').text(car.style)
-			).append(
-				$('<td>').append(
-					$('<a class="resultlink" href="#">').text("Hey That's My Car!")
-				)
-			)
-		return result;
+		return $result;
 	}
 
-})
+});
