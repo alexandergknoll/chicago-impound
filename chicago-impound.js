be12b6445a285216ca2c8cc6e4a582cbd570ce84
Alex Knoll <alexandergknoll@gmail.com>
2015-05-20 18:02:10 -0500

    add recently towed vehicles and clean up code
    shortened date display

--- chicago-impound.js
+++ chicago-impound.js
@@ -1,101 +1,124 @@
-var chicagoImpoundApiURL = "https://data.cityofchicago.org/resource/ygr5-vcbg.json";
+var doc, newCell, searchResults, resultDetail, plateSearchForm, notFound, chicagoImpoundApiURL;
+
+chicagoImpoundApiURL = "https://data.cityofchicago.org/resource/ygr5-vcbg.json";
 
 $(function(){
-	var doc, newCell, searchResults, resultDetail, plateSearchForm, notFound;
-
+	plateSearchForm = $('.searchform');
 	doc = $(document);
+
 	newCell = $('<td>');
+	recents = $('.recent');
 	searchResults = $('.searchresults');
 	resultDetail = $('.resultdetail');
-	plateSearchForm = $('.searchform');
 	notFound = $('.notfound');
+
+	requestRecents();
 
 	plateSearchForm.on('submit', carRequest);
 	doc.on('click','.resultlink', showInventory);
-
-	function carRequest (e) {
-		e.preventDefault();
-
-		var formData = $(this).serialize();
-	 	$.getJSON(chicagoImpoundApiURL, formData).done(carResponse);
-	}
-
-	function showInventory(e) {
-		e.preventDefault();
-
-		var inventoryNumber = $(this).attr("inventory_number");
-		var towedToAddress = $(this).attr("towed_to_address");
-		var towFacilityPhone = $(this).attr("tow_facility_phone");
-
-		resultDetail.text("");
-		appendResultDetail(inventoryNumber, towedToAddress, towFacilityPhone);
-		resultDetail.show();
-	}
-
-	function carResponse (cars) {
-		clearSearchResults();
-		searchResults.hide();
-		notFound.hide();
-		resultDetail.hide();
-		if (cars.length > 0) {
-			cars.forEach(appendCarResult);
-			searchResults.show();
-		} else {
-			notFound.show();
-		}
-	}
-
-	function clearSearchResults () {
-		searchResults.find('.result').remove();
-	}
-
-	function appendCarResult (car) {
-		htmlCarResult(car).appendTo(searchResults);
-	}
-
-	function appendResultDetail (inventoryNumber, towedToAddress, towFacilityPhone) {
-		mapsURL = "http://maps.googleapis.com/maps/api/staticmap?center=" + encodeURI(towedToAddress) + ",%20Chicago,%20IL&zoom=12&size=420x420&maptype=roadmap&markers=color:red%7Clabel:A%7C" + encodeURI(towedToAddress) + ",%20Chicago,%20IL&sensor=false"
-
-		resultDetail
-			.append($('<h3>')
-				.text("Found it!"))
-			.append($('<p>')
-				.text("Inventory Number: " + inventoryNumber))
-			.append($('<p>')
-				.text(towedToAddress))
-			.append($('<p>')
-				.text(towFacilityPhone))
-			.append($('<img>')
-				.attr({
-					src: mapsURL
-				})
-			);
-	}
-
-	function htmlCarResult (car) {
-		var $result = $('<tr class="result">');
-
-		$result
-				.append($('<td>').text(car.tow_date.substring(0,10)))
-				.append($('<td>').text(car.plate))
-				.append($('<td>').text(car.state))
-				.append($('<td>').text(car.make))
-				.append($('<td>').text(car.color))
-				.append($('<td>').text(car.style))
-				.append($('<td>')
-					.append($('<a>')
-						.text("Hey That's My Car!")
-						.attr({
-							class: "resultlink",
-							href: "#",
-							inventory_number: car.inventory_number,
-							towed_to_address: car.towed_to_address,
-							tow_facility_phone: car.tow_facility_phone
-						})
-					)
-				);
-
-		return $result;
-	}
-
 });
+
+function requestRecents() {
+	var recentsURL = chicagoImpoundApiURL + "?$order=tow_date%20DESC&$limit=3";
+	$.getJSON(recentsURL).done(recentsResults);
+}
+
+function recentsResults(r) {
+	console.log(r);
+	r.forEach(appendRecent);
+}
+
+function appendRecent(r) {
+	var recentResult = r.plate + " " + r.state + " "
+	recents.append(recentResult);
+}
+
+function carRequest(e) {
+	e.preventDefault();
+
+	var formData = $(this).serialize();
+ 	$.getJSON(chicagoImpoundApiURL, formData).done(carResponse);
+}
+
+function showInventory(e) {
+	e.preventDefault();
+
+	var inventoryNumber = $(this).attr("inventory_number");
+	var towedToAddress = $(this).attr("towed_to_address");
+	var towFacilityPhone = $(this).attr("tow_facility_phone");
+
+	resultDetail.text("");
+	appendResultDetail(inventoryNumber, towedToAddress, towFacilityPhone);
+	resultDetail.show();
+}
+
+function carResponse(r) {
+	clearSearchResults();
+	recents.hide();
+	searchResults.hide();
+	notFound.hide();
+	resultDetail.hide();
+	if (r.length > 0) {
+		r.forEach(appendCarResult);
+		searchResults.show();
+	} else {
+		notFound.show();
+	}
+}
+
+function clearSearchResults() {
+	searchResults.find('.result').remove();
+}
+
+function appendCarResult(r) {
+	htmlCarResult(r).appendTo(searchResults);
+}
+
+function appendResultDetail(inventoryNumber, towedToAddress, towFacilityPhone) {
+	mapsURL = "http://maps.googleapis.com/maps/api/staticmap?center=" 
+						+ encodeURI(towedToAddress) 
+					  + ",%20Chicago,%20IL&zoom=12&size=420x420&maptype=roadmap&markers=color:red%7C" 
+					  + encodeURI(towedToAddress)
+					  + ",%20Chicago,%20IL&sensor=false"
+
+	resultDetail
+		.append($('<h3>')
+			.text("Found it!"))
+		.append($('<p>')
+			.text("Inventory Number: " + inventoryNumber))
+		.append($('<p>')
+			.text(towedToAddress))
+		.append($('<p>')
+			.text(towFacilityPhone))
+		.append($('<img>')
+			.attr({
+				src: mapsURL
+			})
+		);
+}
+
+function htmlCarResult(r) {
+	var $result = $('<tr class="result">');
+
+	$result
+			.append($('<td>').text(r.tow_date.substring(5,10)))
+			.append($('<td>').text(r.plate))
+			.append($('<td>').text(r.state))
+			.append($('<td>').text(r.make))
+			.append($('<td>').text(r.color))
+			.append($('<td>').text(r.style))
+			.append($('<td>')
+				.append($('<a>')
+					.text("Hey That's My Car!")
+					.attr({
+						class: "resultlink",
+						href: "#",
+						inventory_number: r.inventory_number,
+						towed_to_address: r.towed_to_address,
+						tow_facility_phone: r.tow_facility_phone
+					})
+				)
+			);
+
+	return $result;
+}
